a53e542ecff9f52e26e5f8fccf453360832ef8e7
Mahdi <mmahdikarimi2006@gmail.com>
2025-01-07 08:14:03 +0430

    setup files added

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import Header from "@/components/layout/header";
+import Footer from "@/components/layout/footer";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Mahdi Karimi",
+  description: "Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ components/layout/header.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Header() {
+  return (
+    <section>
+      <button>
+        <Link href="/">link</Link>
+      </button>
+    </section>
+  );
+}
