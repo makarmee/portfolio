bb80b4799a9654a71aa89cdc819513fec407be2c
Mahdi <mmahdikarimi2006@gmail.com>
2025-01-08 10:14:35 +0430

    layout managed

--- components/layout/menu.tsx
+++ components/layout/menu.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 export function Menu() {
   const menu = [
-    { id: 1, name: "", icon: <SquareUserIcon className="w-5 h-5" /> },
+    { id: 1, name: "about", icon: <SquareUserIcon className="w-5 h-5" /> },
     { id: 2, name: "skills", icon: <SparkleIcon className="w-5 h-5" /> },
     { id: 3, name: "projects", icon: <RocketIcon className="w-5 h-5" /> },
     { id: 4, name: "contacts", icon: <MailOpenIcon className="w-5 h-5" /> },
