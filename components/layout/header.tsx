cf3794d5ec1ae622857590f8192e9a719938403b
makarmee <mmahdikarimi2006@gmail.com>
2025-07-27 15:40:15 +0430

    small changes

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Menu } from "./menu";
-import { HomeIcon } from "lucide-react";
 
 export default function Header() {
   return (
     <header className="bg-neutral-800/50 backdrop-blur-3xl backdrop-brightness-75 border-b border-neutral-700 sticky w-full top-0 z-20">
       <div className="max-w-screen-sm mx-auto flex justify-between items-center h-14 px-8 md:px-0">
-        {/* <HomeIcon className="w-5 h-5" /> */}
         <h1 className="mb-2 text-2xl font-black pt-2">M</h1>
         <Menu />
       </div>
